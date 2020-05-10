4b14f7da3e7f2379ce73b305e288dad6b7c479b3
carlrietschel <carlrietschel@gmail.com>
2020-05-10 20:51:13 +0200

    add utils_eval

---
+++ utils_eval.py
@@ -0,0 +1,47 @@
+'''
+Implemented: 02/12/2018
+  > For survival analysis evaluation
+
+First implemented by Kartik Ahuja
+Modified by CHANGHEE LEE
+
+Modifcation List:
+	- Brier Score added ()
+'''
+
+import numpy as np
+
+### C(t)-INDEX CALCULATION
+def c_index(Prediction, Time_survival, Death, Time):
+    '''
+        This is a cause-specific c(t)-index
+        - Prediction      : risk at Time (higher --> more risky)
+        - Time_survival   : survival/censoring time
+        - Death           :
+            > 1: death
+            > 0: censored (including death from other cause)
+        - Time            : time of evaluation (time-horizon when evaluating C-index)
+    '''
+    N = len(Prediction)
+    A = np.zeros((N,N))
+    Q = np.zeros((N,N))
+    N_t = np.zeros((N,N))
+    Num = 0
+    Den = 0
+    for i in range(N):
+        A[i, np.where(Time_survival[i] < Time_survival)] = 1
+        Q[i, np.where(Prediction[i] > Prediction)] = 1
+  
+    for i in range(N):
+        if (Time_survival[i]<=Time and Death[i]==1):
+            N_t[i,:] = 1
+
+    Num  = np.sum(((A)*N_t)*Q)
+    Den  = np.sum((A)*N_t)
+
+    if Num == 0 and Den == 0:
+        result = -1 # not able to compute c-index!
+    else:
+        result = float(Num/Den)
+
+    return result
